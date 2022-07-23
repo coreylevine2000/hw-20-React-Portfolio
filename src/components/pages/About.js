be3fc2cf47f8aaeea1ae98546b7190a61f4fc5fe
Corey Levine <coreylevine2000@gmail.com>
2022-07-22 20:55:41 -0400

    adding content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,21 +1,16 @@
 import React from 'react';
 
-const styles = {
-
-};
 
 
 export default function About() {
   return (
-    <div>
-      <h1 style={styles} className="about">Alittle About Myself</h1>
+    <div className='card'>
+      <h1>Alittle About Myself</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+Website design and development has always been something that has facinated me as I was growing up, and now have finally been able to persue said interest, thanks in part to my supporting family. 
+      </p>
+      <p>
+        I am a graduate of Florida Gulf Coast University and have living in the Atlanta area for the past several years. Always working with a team, it is exciting to meet and learn about new people, and thanks main in part to the Georgia Tech Boot Camp for coding, I am able to explore the puzzle of website developement.
       </p>
     </div>
   );
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,21 +1,45 @@
 import React from 'react';
+import AMDB from '../images/AMDB.png';
 
-const styles = {
-  mainStyle: {
-    background: 'white',
-  },
-  bodyStyle: {
-    fontSize: '20px',
-  },
-};
 
 export default function Project() {
+  
+  const projectList = [
+    {
+      name: "Actor Movie Database",
+      webpage: "https://vi3t4lov3.github.io/GRP12-The-Movie-Finder/",
+      repo: "https://github.com/vi3t4lov3/GRP12-The-Movie-Finder",
+      screenshot: AMDB
+    },
+  ]
+
+  const ProjectItem = ({list}) => (
+    <ul className="repolist">
+      {list.map(item =>
+        <li>
+            <p>{item.name}</p>
+            <a
+              target="_blank"
+              href={item.webpage}
+              >Webpage</a>
+            <a
+              target="_blank"
+              href={item.repo}
+              >Git Repo</a>
+            <a target="_blank" href={item.screenshot}
+              >Preview</a>
+          </li>
+      )}
+    </ul>  
+  );
+
   return (
     <div>
-      <h1 style={styles.mainStyle}>Current and Previous projects</h1>
-      <p style={styles.bodyStyle}>
-        The following images are usable links to current and completed projects and challenges. Each link will direct you to its respected repository on Github with additional instructions on how to launch the application and/or a link to the application's live website. 
+      <h1>Current and Previous projects</h1>
+      <p>
+Enjoy
       </p>
+      <ProjectItem list={projectList}/>
     </div>
   );
 }
